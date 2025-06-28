6c88cbcda761d2e901375d77891f0e0cff5f358a
unknown <abdallah.medhata3@gmail.com>
2025-06-28 19:14:07 +0300

    Fixing Some issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+// Start Global Variables
 let select = document.getElementById("spec");
-let options = [...select.children];
-select.oninput = function () {
-    console.log(select.value);
-    location.href = select.value;
+if (select) {
+    let options = [...select.children];
+}
+let showPass = document.getElementsByClassName("fa-eye")[0];
+// Start Global Variables
+if (select) {
+    select.oninput = function () {
+        console.log(select.value);
+        location.href = select.value;
+    }
+}
+if (showPass) {
+    showPass.onclick = function () {
+        if (showPass.previousElementSibling.type == "password") {
+            showPass.previousElementSibling.type = "text";
+        } else {
+            showPass.previousElementSibling.type = "password";
+        }
+    }
 }
